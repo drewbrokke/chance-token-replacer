076a3ecaf9c66e97d7ff5003b1182827e4c74eeb
drewbrokke <drew.brokke@liferay.com>
2016-02-11 09:22:23 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,118 @@
+var Chance = require('chance');
+
+var chance = new Chance();
+var _ = require('lodash');
+
+function TokenReplacer() {
+	this.evaluatedTokens = [];
+}
+
+TokenReplacer.prototype = {
+
+	cacheEvaluatedToken: function(s) {
+		this.evaluatedTokens.push(s);
+	},
+
+	callMethod: function(methodName, configurationObject) {
+		return chance[methodName](configurationObject);
+	},
+
+	getArray: function(token) {
+		return this.stripAmpersands(token).split('#');
+	},
+
+	getConfigurationObject: function(token) {
+		var configurationObject = null;
+		var configurationString = this.getConfigurationString(token);
+
+		if (!!configurationString) {
+			var configurationArray = this.splitByComma(configurationString);
+
+			configurationArray = _.map(configurationArray, this.splitByColon);
+
+			configurationObject = _.fromPairs(configurationArray);
+		}
+
+		return configurationObject;
+	},
+
+	getConfigurationString: function(token) {
+		return this.stripOuterCurlyBraces(this.getArray(token)[1]);
+	},
+
+	getMethodName: function(token) {
+		return this.getArray(token)[0];
+	},
+
+	processString: function(s) {
+		s = this.replaceTokens(s);
+		s = this.replaceBackreferences(s);
+
+		return s;
+	},
+
+	replaceBackreference: function(s) {
+		var index = Number(this.trimLeftBang(s)) - 1;
+
+		return this.evaluatedTokens[index];
+	},
+
+	replaceBackreferences: function(s) {
+		return s.replace(this.PATTERN_BACKREFERENCE, this.replaceBackreference.bind(this));
+	},
+
+	replaceToken: function(token) {
+		var methodName = this.getMethodName(token);
+		var configurationObject = this.getConfigurationObject(token);
+
+		var evaluatedToken = this.callMethod(methodName, configurationObject);
+
+		this.cacheEvaluatedToken(evaluatedToken);
+
+		return evaluatedToken;
+	},
+
+	replaceTokens: function(s) {
+		return s.replace(this.PATTERN_TOKEN, this.replaceToken.bind(this));
+	},
+
+	resetEvaluatedTokens: function() {
+		this.evaluatedTokens = [];
+	},
+
+	splitByComma: function(s) {
+		return s.split(',');
+	},
+
+	splitByColon: function(s) {
+		return s.split(':');
+	},
+
+	stripAmpersands: function(token) {
+		token = _.trimStart(token, '<@');
+		token = _.trimEnd(token, '@>');
+
+		return token;
+	},
+
+	stripOuterCurlyBraces: function(s) {
+		s = _.trimStart(s, '{');
+		s = _.trimEnd(s, '}');
+
+		return s;
+	},
+
+	trimLeftBang: function(s) {
+		return _.trimStart(s, '!');
+	},
+
+	trimOuterWhitespace: function(s) {
+		return _.trim(s, ' ');
+	},
+
+	PATTERN_BACKREFERENCE: /!\d/gi,
+	PATTERN_TOKEN: /<@.+?@>/gi
+
+};
+
+module.exports = TokenReplacer;
